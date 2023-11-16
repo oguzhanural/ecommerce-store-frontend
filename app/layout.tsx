aa7fb1aaa9d47ff4779f19755c44780e10ee4287
oguzhanural <oguzhnural@gmail.com>
2023-11-16 15:47:34 +0300

    updated layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next'
 import { Urbanist } from 'next/font/google'
 import './globals.css'
-import Footer from './components/footer'
-import Navbar from './components/navbar'
+import Footer from '@/components/footer'
+import Navbar from '@/components/navbar'
 
 const font = Urbanist({ subsets: ['latin'] })
 
