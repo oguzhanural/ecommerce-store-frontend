651ca4e964c61f630dc26dbfeca778720d10fa7b
oguzhanural <oguzhnural@gmail.com>
2023-11-16 15:47:22 +0300

    added footer

---
+++ components/footer.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='bg-white border-t'>
+        <div className="mx-auto py-10">
+            <p className='text-center text-xs text-black'>
+                &copy; 2023 oguzhanural.com, Inc All rights reserved.
+            </p>
+
+        </div>
+
+    </footer>
+  )
+}
+
+export default Footer;
