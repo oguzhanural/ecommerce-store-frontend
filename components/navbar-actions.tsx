57523d2b8a9e45e6ece24eba8bc9d144a2d00809
oguzhanural <oguzhnural@gmail.com>
2023-11-18 20:51:24 +0300

    added navbar actions

---
+++ components/navbar-actions.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+const NavbarActions = () => {
+    
+  return (
+    <div className="ml-auto flex items-center gap-x-4">
+        NavbarActions
+    </div>
+  )
+}
+
+export default NavbarActions
