a7db05f905f956fc12c3754c196618997a60fed0
oguzhanural <oguzhnural@gmail.com>
2023-11-19 12:04:28 +0300

    added Container and Billboard component on page

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,10 +1,16 @@
+import Billboard from '@/components/billboard'
+import Container from '@/components/ui/container'
 import React from 'react'
 
 const HomePage = () => {
 
   return (
     <div>
-        Storeeee
+        <Container>
+          <div className="space-y-10 pb-10">
+            <Billboard />
+          </div>
+        </Container>
 
     </div>
   )
