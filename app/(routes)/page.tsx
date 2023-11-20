1ddece417d96df76d9e26826a4d1b72d74c1a6a9
oguzhanural <oguzhnural@gmail.com>
2023-11-20 10:29:21 +0300

    added billboard data prop

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,14 +1,21 @@
+import getBillboard from '@/actions/get-billboards';
 import Billboard from '@/components/billboard'
 import Container from '@/components/ui/container'
 import React from 'react'
 
-const HomePage = () => {
+export const revalidate = 0;
+
+const HomePage = async () => {
+
+  const billboard = await getBillboard('109c3b82-a275-4a39-80a3-7e09ee5b52d3');
 
   return (
     <div>
         <Container>
           <div className="space-y-10 pb-10">
-            <Billboard />
+            <Billboard
+              data={billboard}
+            />
           </div>
         </Container>
 
