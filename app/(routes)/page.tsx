744088859d8b1c040894fcdc95a18a529a4f57b3
oguzhanural <oguzhnural@gmail.com>
2023-11-22 10:34:11 +0300

    updated home page

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,12 +1,15 @@
 import getBillboard from '@/actions/get-billboards';
+import getProducts from '@/actions/get-products';
 import Billboard from '@/components/billboard'
+import ProductList from '@/components/product-list';
 import Container from '@/components/ui/container'
 import React from 'react'
 
 export const revalidate = 0;
 
 const HomePage = async () => {
-
+  
+  const products = await getProducts( { isFeatured: true } );
   const billboard = await getBillboard('109c3b82-a275-4a39-80a3-7e09ee5b52d3');
 
   return (
@@ -16,6 +19,9 @@ const HomePage = async () => {
             <Billboard
               data={billboard}
             />
+          </div>
+          <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+            <ProductList title="Featured Products" items={products} />
           </div>
         </Container>
 
