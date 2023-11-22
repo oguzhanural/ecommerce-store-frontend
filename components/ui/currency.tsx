9934e26864a480470122e5d560cea4c053c1d7d2
oguzhanural <oguzhnural@gmail.com>
2023-11-22 13:33:23 +0300

    added github some files

---
+++ components/ui/currency.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+const formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD"
+  });
+
+interface CurrencyProps{
+    value?: string | number;
+}
+
+const Currency:React.FC<CurrencyProps> = ({value}) => {
+
+    
+
+  return (
+    <div className="font-semibold">
+        {formatter.format(Number(value))}
+    </div>
+  )
+}
+
+export default Currency;
